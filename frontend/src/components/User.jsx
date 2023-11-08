4455b367b6f80e5c6f24f2251d3e38668fe0d10e
LonelySam16 <eagleray962@gmail.com>
2023-11-09 02:26:33 +0530

    user info page sexified

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -1,19 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
-
-const UserInfo = () => {
-  const {user} = useAuth()
+const User = () => {
+  const { user } = useAuth();
   const [userData, setUserData] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        if(user){
+        if (user) {
           setUserData(user);
           return;
         }
-        const response = await axios.get('http://localhost:5000/api/v1/users/me',{withCredentials:true});
+        const response = await axios.get('http://localhost:5000/api/v1/users/me', { withCredentials: true });
 
         if (response.data.status === 'success') {
           setUserData(response.data.data);
@@ -26,40 +25,48 @@ const UserInfo = () => {
     };
 
     fetchData();
-  }, []); 
+  }, []);
 
   return (
-    <div className="w-1/2 pt-10 px-10">
-      <div className='border border-black w-min w-96 h-96 grid place-items-center'>
-      <h1 className='font-bold text-xl'>Profile Information</h1>
-        <div className='-mt-32'>
-        {userData && (
-          <>
-            <div className="flex gap-1 mb-4">
-              <p className="text-sm font-semibold mb-1">Name:</p>
-              <p className="text-sm capitalize">{userData.first_name} {userData.last_name}</p>
-            </div>
+    <div className="relative w-full pt-10 px-10 bg-cover bg-center" style={{ backgroundImage: "url('./userinfoback.jpg')" }}>
+      <div className="absolute inset-0 bg-black bg-opacity-70"></div>
+      <div className="relative w-full h-96 grid place-items-center text-center">
+        <h1 className="font-bold text-5xl text-white">
+          {userData && userData.first_name.toUpperCase()} {userData && userData.last_name.toUpperCase()}
+        </h1>
 
-            <div className="flex gap-1 mb-4">
-              <label className="block text-sm font-semibold mb-1">Phone Number:</label>
-              <p className="text-sm">{userData.phone}</p>
-            </div>
+        <div className="-mt-32 text-white">
+          {userData && (
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <div className="flex gap-1 mb-4">
+                  <p className="text-xl font-semibold mb-1">Roll No: &nbsp;</p>
+                  <p className="text-xl uppercase">{userData.roll}</p>
+                </div>
 
-            <div className="flex gap-1 mb-4">
-              <label className="block text-sm font-semibold mb-1">Address:</label>
-              <p className="text-sm capitalize">{userData.address}</p>
-            </div>
+                <div className="flex gap-1 mb-4">
+                  <label className="block text-xl font-semibold mb-1">Phone Number: &nbsp;</label>
+                  <p className="text-xl">{userData.phone}</p>
+                </div>
+              </div>
 
-            <div className="flex gap-1 mb-4">
-              <label className="block text-sm font-semibold mb-1">Email:</label>
-              <p className="text-sm">{userData.email}</p>
+              <div>
+                <div className="flex gap-1 mb-4">
+                  <label className="block text-xl font-semibold mb-1">Address: &nbsp;</label>
+                  <p className="text-xl capitalize">{userData.address}</p>
+                </div>
+
+                <div className="flex gap-1 mb-4">
+                  <label className="block text-xl font-semibold mb-1">Email: &nbsp;</label>
+                  <p className="text-xl">{userData.email}</p>
+                </div>
+              </div>
             </div>
-          </>
-        )}
+          )}
         </div>
       </div>
     </div>
   );
 };
 
-export default UserInfo;
+export default User;

a71eccef8a3b2e417c526cdc65458cb40a33825a
LonelySam16 <eagleray962@gmail.com>
2023-11-11 01:58:04 +0530

    header distortion corrected, Hamburger icon added

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 
 const Header = () => {
   const { user, logout: authLogout } = useAuth();
+  const [display, setDisplay] = useState(false);
 
   const handleLogout = () => {
     authLogout();
   }
 
+  const handleDisplay = () => {
+    setDisplay(!display);
+  }
+
   return (
-    <section className="flex items-center bg-[#9CFF88] justify-center px-56" style={{ height: '10vh' }}>
-      <div className="w-1/2 flex">
+    <section className="flex items-center bg-[#9CFF88] justify-between px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-16" style={{ height: '10vh' }}>
+      <div className="flex items-center">
         <Link to="/">
           <img src="http://localhost:5173/VcommerceLogo.png" alt="Logo" width={130} />
         </Link>
       </div>
-        <div className="gap-10 flex justify-end w-1/2 text-[#2F4858]">
-          {user && <Link to="/userinfo">User Info</Link>}
-          <Link to="/products">Products</Link>
-          {user && <Link to="/addlisting">Add Products</Link>}
-          {user && <Link to="/favourites">Favourites</Link>}
-          {!user && <Link to="/login">Login</Link>}
-          {user && <button className="inline-block h-min" onClick={handleLogout}>Logout</button>}
-        </div>
+      <div className="flex gap-4">
+        {display && (
+          <div className="flex items-center gap-4 text-[#2F4858]">
+            {user && <Link to="/userinfo">User Info</Link>}
+            <Link to="/products">Products</Link>
+            {user && <Link to="/addlisting">Add Products</Link>}
+            {user && <Link to="/favourites">Favourites</Link>}
+            {!user && <Link to="/login">Login</Link>}
+            {user && <button className="inline-block h-min" onClick={handleLogout}>Logout</button>}
+          </div>
+        )}
+        <button onClick={handleDisplay}>
+          <img src="./public/hamburger.png" alt="Hamburger" width={30} />
+        </button>
+      </div>
     </section>
   );
 };

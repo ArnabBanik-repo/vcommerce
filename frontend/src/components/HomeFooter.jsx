56540dc46bad71130ac235dd384d96492145631b
LonelySam16 <eagleray962@gmail.com>
2023-11-15 17:26:03 +0530

    footer update

--- frontend/src/components/HomeFooter.jsx
+++ frontend/src/components/HomeFooter.jsx
@@ -18,7 +18,7 @@ const HomeFooter = () => {
             <FaInstagram size={30} />
             <FaTwitterSquare size={30} />
             <FaGithubSquare size={30} />
-            <FaDribbbleSquare size={30} />
+        
         </div>
       </div>
       <div className='lg:col-span-2 flex justify-around mt-6'>
@@ -41,15 +41,25 @@ const HomeFooter = () => {
         </ul>
     </div>
     <div>
-        <h6 className='font-medium text-gray-400'>Company</h6>
-        <ul>
-            <li className='py-2 text-sm'>About</li>
-            <li className='py-2 text-sm'>Blog</li>
-            <li className='py-2 text-sm'>Jobs</li>
-            <li className='py-2 text-sm'>Press</li>
-            <li className='py-2 text-sm'>Careers</li>
-        </ul>
-    </div>
+  <h6 className='font-medium text-gray-400'>About us </h6>
+  Just two guys trying to make it work... <br />
+  Connect with us personally at:
+  <ul style={{ listStyle: 'none', padding: 0 }}>
+    <li className='py-2 text-sm flex items-center'>
+      <a href='https://www.instagram.com/arnabbanik5/' target='_blank' rel='noopener noreferrer' className='flex items-center'>
+        <FaInstagram size={30} className='mr-2' />
+        Arnab
+      </a>
+    </li>
+    <li className='py-2 text-sm flex items-center'>
+      <a href='https://www.instagram.com/rayankan16/' target='_blank' rel='noopener noreferrer' className='flex items-center'>
+        <FaInstagram size={30} className='mr-2' />
+        Ankan
+      </a>
+    </li>
+  </ul>
+</div>
+
   
       </div>
     </div>

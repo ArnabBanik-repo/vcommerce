80b96c8e77e075daa0a32a78c5361b7fbaf51fa3
Arnab Banik <arnabbanik9@gmail.com>
2024-01-10 11:43:20 +0530

    Fixed #4

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -48,7 +48,7 @@ const Header = () => {
         <button className='outline-0 bg-green-500 rounded-full w-10 h-10 flex items-center justify-center p-2 bg-opacity-40 ' onClick={handleDisplay}>
           <img src={Hamburger} alt="Hamburger" width={30} />
         </button>
-        <div className={`top-20 right-10 absolute bg-gray-200 w-44 rounded-md transition-all ${display ? 'block' : 'hidden'}`} style={{zIndex: 100}}>
+        <div className={`top-20 right-10 absolute bg-gray-200 w-44 rounded-md transition-all ${display ? 'block' : 'hidden'}`} style={{zIndex: 100}} onClick={() => handleDisplay()}>
           {user && <Link className='h-16 px-4 flex items-center border-b border-gray-400 hover:bg-gray-300 rounded-md cursor-pointer transition-all' to="/userinfo">User Info</Link>}
           <Link className='h-16 px-4 flex items-center border-b border-gray-400 hover:bg-gray-300 rounded-md cursor-pointer transition-all' to="/products">Products</Link>
           {user && <Link className='h-16 px-4 flex items-center border-b border-gray-400 hover:bg-gray-300 rounded-md cursor-pointer transition-all' to="/addlisting">Add Products</Link>}
